a37b0646623c1f2aac65c62195ea12ff5f06ae2d
yuanho-0211 <ho950211@gmail.com>
2025-11-23 01:33:21 +0800

    change formula

--- AssignmentIV/C/hash_fn.c
+++ AssignmentIV/C/hash_fn.c
@@ -43,6 +43,7 @@ int myHashString(const char* str, int m) {
         hash = (hash + (unsigned char)(str[i]) * pi);
         pi = (pi * p)  ; // next p^i
     }
-    int hash_new = (hash * 7 ) % m;
+    int hash_new = ((hash+1) * 17 ) % m;
+    hash_new = ((hash+1) * 13 ) % m;
     return hash_new ; // string hashing 
 }
